3ef97a2c64c26404d9282a8e27421e04834c7183
Dworv <dwarvyt@gmail.com>
2023-10-20 13:48:19 -0700

    feat: single out keypresses

--- speedtest/src/lib.rs
+++ speedtest/src/lib.rs
@@ -34,25 +34,76 @@ pub fn row_container_ui(offset: Val) -> NodeBundle {
 }
 
 pub fn add_key(builder: &mut ChildBuilder, key: char, font: Handle<Font>) {
-    builder.spawn(NodeBundle {
-        style: Style {
-            height: Val::Percent(100.),
-            aspect_ratio: Some(1.),
-            align_items: AlignItems::Center,
-            justify_content: JustifyContent::Center,
-            // padding: UiRect::all(Val::Vh(2.5)),
-            ..Default::default()
-        },
-        background_color: Color::rgb(0.2, 0.2, 0.5).into(),
-        ..Default::default()
-    }).with_children(|parent| {
-        parent.spawn(TextBundle::from_section(
-            key,
-            TextStyle {
-                font,
-                font_size: 40.0,
-                color: Color::rgb(0.9, 0.9, 0.9),
+    builder
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    height: Val::Percent(100.),
+                    aspect_ratio: Some(1.),
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::Center,
+                    // padding: UiRect::all(Val::Vh(2.5)),
+                    ..Default::default()
+                },
+                background_color: Color::rgb(0.2, 0.2, 0.5).into(),
+                ..Default::default()
             },
-        ).with_text_alignment(TextAlignment::Center));
-    });
+            Key {
+                keycode: char_to_keycode(key),
+            },
+        ))
+        .with_children(|parent| {
+            parent.spawn(
+                TextBundle::from_section(
+                    key,
+                    TextStyle {
+                        font,
+                        font_size: 40.0,
+                        color: Color::rgb(0.9, 0.9, 0.9),
+                    },
+                )
+                .with_text_alignment(TextAlignment::Center),
+            );
+        });
+}
+
+#[derive(Component)]
+pub struct Key {
+    pub keycode: KeyCode,
+}
+
+pub fn char_to_keycode(c: char) -> KeyCode {
+    match c {
+        'A' => KeyCode::A,
+        'B' => KeyCode::B,
+        'C' => KeyCode::C,
+        'D' => KeyCode::D,
+        'E' => KeyCode::E,
+        'F' => KeyCode::F,
+        'G' => KeyCode::G,
+        'H' => KeyCode::H,
+        'I' => KeyCode::I,
+        'J' => KeyCode::J,
+        'K' => KeyCode::K,
+        'L' => KeyCode::L,
+        'M' => KeyCode::M,
+        'N' => KeyCode::N,
+        'O' => KeyCode::O,
+        'P' => KeyCode::P,
+        'Q' => KeyCode::Q,
+        'R' => KeyCode::R,
+        'S' => KeyCode::S,
+        'T' => KeyCode::T,
+        'U' => KeyCode::U,
+        'V' => KeyCode::V,
+        'W' => KeyCode::W,
+        'X' => KeyCode::X,
+        'Y' => KeyCode::Y,
+        'Z' => KeyCode::Z,
+        ';' => KeyCode::Semicolon,
+        ',' => KeyCode::Comma,
+        '.' => KeyCode::Period,
+        '/' => KeyCode::Slash,
+        _ => panic!("no keycode???? :O")
+    }
 }
--- speedtest/src/main.rs
+++ speedtest/src/main.rs
@@ -1,11 +1,12 @@
 use bevy::prelude::*;
 
-use speedtest::{root_ui, row_container_ui, add_key};
+use speedtest::{add_key, root_ui, row_container_ui, Key};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (setup_camera, setup_keyboard))
+        .add_systems(Update, highlight_key)
         .run();
 }
 
@@ -16,41 +17,59 @@ fn setup_camera(mut commands: Commands) {
 fn setup_keyboard(mut commands: Commands, asset_server: Res<AssetServer>) {
     let font = asset_server.load("fonts/FiraSans-Bold.ttf");
     commands.spawn(root_ui()).with_children(|parent| {
-        parent.spawn(row_container_ui(Val::Vh(0.))).with_children(|parent| {
-            add_key(parent, 'Q', font.clone());
-            add_key(parent, 'W', font.clone());
-            add_key(parent, 'E', font.clone());
-            add_key(parent, 'R', font.clone());
-            add_key(parent, 'T', font.clone());
-            add_key(parent, 'Y', font.clone());
-            add_key(parent, 'U', font.clone());
-            add_key(parent, 'I', font.clone());
-            add_key(parent, 'O', font.clone());
-            add_key(parent, 'P', font.clone());
-        });
-        parent.spawn(row_container_ui(Val::Vh(2.5))).with_children(|parent| {
-            add_key(parent, 'A', font.clone());
-            add_key(parent, 'S', font.clone());
-            add_key(parent, 'D', font.clone());
-            add_key(parent, 'F', font.clone());
-            add_key(parent, 'G', font.clone());
-            add_key(parent, 'H', font.clone());
-            add_key(parent, 'J', font.clone());
-            add_key(parent, 'K', font.clone());
-            add_key(parent, 'L', font.clone());
-            add_key(parent, ';', font.clone());
-        });
-        parent.spawn(row_container_ui(Val::Vh(7.5))).with_children(|parent| {
-            add_key(parent, 'Z', font.clone());
-            add_key(parent, 'X', font.clone());
-            add_key(parent, 'C', font.clone());
-            add_key(parent, 'V', font.clone());
-            add_key(parent, 'B', font.clone());
-            add_key(parent, 'N', font.clone());
-            add_key(parent, 'M', font.clone());
-            add_key(parent, ',', font.clone());
-            add_key(parent, '.', font.clone());
-            add_key(parent, '/', font.clone());
-        });
+        parent
+            .spawn(row_container_ui(Val::Vh(0.)))
+            .with_children(|parent| {
+                add_key(parent, 'Q', font.clone());
+                add_key(parent, 'W', font.clone());
+                add_key(parent, 'E', font.clone());
+                add_key(parent, 'R', font.clone());
+                add_key(parent, 'T', font.clone());
+                add_key(parent, 'Y', font.clone());
+                add_key(parent, 'U', font.clone());
+                add_key(parent, 'I', font.clone());
+                add_key(parent, 'O', font.clone());
+                add_key(parent, 'P', font.clone());
+            });
+        parent
+            .spawn(row_container_ui(Val::Vh(2.5)))
+            .with_children(|parent| {
+                add_key(parent, 'A', font.clone());
+                add_key(parent, 'S', font.clone());
+                add_key(parent, 'D', font.clone());
+                add_key(parent, 'F', font.clone());
+                add_key(parent, 'G', font.clone());
+                add_key(parent, 'H', font.clone());
+                add_key(parent, 'J', font.clone());
+                add_key(parent, 'K', font.clone());
+                add_key(parent, 'L', font.clone());
+                add_key(parent, ';', font.clone());
+            });
+        parent
+            .spawn(row_container_ui(Val::Vh(7.5)))
+            .with_children(|parent| {
+                add_key(parent, 'Z', font.clone());
+                add_key(parent, 'X', font.clone());
+                add_key(parent, 'C', font.clone());
+                add_key(parent, 'V', font.clone());
+                add_key(parent, 'B', font.clone());
+                add_key(parent, 'N', font.clone());
+                add_key(parent, 'M', font.clone());
+                add_key(parent, ',', font.clone());
+                add_key(parent, '.', font.clone());
+                add_key(parent, '/', font.clone());
+            });
     });
+}
+
+fn highlight_key(mut query: Query<(&mut BackgroundColor, &Key)>, input: Res<Input<KeyCode>>) {
+    for (mut color, key) in &mut query {
+        if input.just_pressed(key.keycode) {
+            println!("key pressed: {:?}", key.keycode);
+            color.0 = Color::rgb(0.5, 0.2, 0.5);
+        }
+        if input.just_released(key.keycode) {
+            color.0 = Color::rgb(0.2, 0.2, 0.5);
+        }
+    }
 }
