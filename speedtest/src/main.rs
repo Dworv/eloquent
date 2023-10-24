ffd947c14e879a44aa57c0ff362739a508bfe841
Dworv YT <dwarvyt@gmail.com>
2023-10-24 13:13:01 -0700

    feat: clear old impoorts

--- speedtest/src/main.rs
+++ speedtest/src/main.rs
@@ -3,5 +3,10 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup_camera)
         .run()
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
